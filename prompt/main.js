d1c51e86ccc9f56b1bd9a82ba2e0f7d61388bffd
khumaryn <119412655+khumaryn@users.noreply.github.com>
2023-02-15 14:07:28 +0400

    Add files via upload

---
+++ prompt/main.js
@@ -0,0 +1,12 @@
+function pro(){
+let age=prompt("Yasinizi daxil edin")    
+
+    if(age>=18){
+       alert('davam ede bilersiz ') 
+
+    }
+    if(age<18){
+         alert('davam ede bilmeyeceksiz')
+            }
+
+}
